18f5ded90c63cc9320ba433400d26534172dbda7
Nickolaj Jepsen <nij@billigvvs.dk>
2022-06-30 20:48:56 +0200

    chore: more setup

---
+++ modules/navbar/NavBar.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+interface INavBarProps {}
+
+const NavBar: FunctionComponent<INavBarProps> = (props) => {
+  return <div></div>;
+};
+
+export default NavBar;
---
+++ theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  color: {
+    background: "#15171b",
+    forground: "#eff0f0",
+    accent: "#cf6a4c",
+  },
+};
+
+export default theme;
